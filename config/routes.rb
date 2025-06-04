ee7e7794d78f1da4aad05ff4ee07f3976d844549
kkraoi <aaoooii219@gmail.com>
2025-06-04 07:34:06 +0000

    [Update]ルーティング調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # ユーザー用: URL /users/sign_in ...
+  # devise_for :users => ユーザーの ログイン、ログアウト、新規登録、パスワード変更など のルーティングやコントローラを自動的に用意する。
+  # skip:オプション => 不要なルーティングを削除
+  # controllers:オプション => 自分で用意したコントローラに差し替える。
+  devise_for :users, skip: [:passwords], controllers: {
+    registrations: "public/registrations", # サインアップやアカウント編集系
+    sessions: 'public/sessions' # ログイン・ログアウト系
+  }
+
+  # 管理者用 URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
 end
