d722bae1a30807dc364fa9f2f19774712829f6a6
kkraoi <aaoooii219@gmail.com>
2025-06-06 06:11:34 +0000

    [Fix]スクロール位置調整

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,3 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -13,20 +8,24 @@ import "channels"
  * @returns {void}
  */
 function setupHeader() {
-  const header = document.getElementById("js-header")
-  const trigger = document.getElementById("js-nav-trigger")
+  const header = document.getElementById("js-header");
+  const trigger = document.getElementById("js-nav-trigger");
   if (!header || !trigger) return;
 
-  const ACTIVE_CLASS_NAME = "is-active"
+  const ACTIVE_CLASS_NAME = "is-active";
 
   trigger.addEventListener("click", () => {
-    header.classList.toggle(ACTIVE_CLASS_NAME)
-  })
+    header.classList.toggle(ACTIVE_CLASS_NAME);
+  });
 }
 
+/**
+ * トップ位置にスクロールする処理
+ * @returns {void}
+ */
 function setupScrollToTop() {
-  const trigger = document.getElementById("js-to-top")
-  if(!trigger) return;
+  const trigger = document.getElementById("js-to-top");
+  if (!trigger) return;
 
   trigger.addEventListener("click", () => {
     window.scrollTo({
