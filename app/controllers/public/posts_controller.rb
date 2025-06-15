15d252b6543e7dcf0c28c09aa6d7cb530a0bf686
kkraoi <aaoooii219@gmail.com>
2025-06-15 06:57:37 +0000

    [Add]投稿作成ページ作成

---
+++ app/controllers/public/posts_controller.rb
@@ -0,0 +1,45 @@
+class Public::PostsController < ApplicationController
+  def index
+    
+  end
+  
+  def show
+    
+  end
+  
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    post = Post.new(post_params)
+    post.user_id = current_user.id
+    if post.save
+      redirect_to post_path(post), notice: "投稿に成功しました"
+    else
+      render "new", alert: "投稿に失敗しました"
+    end
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    
+  end
+  
+  def destroy
+    
+  end
+  
+  private
+
+  def post_params
+    params.require(:post).permit(
+      :title,
+      :summary,
+      :description
+    )
+  end
+end
