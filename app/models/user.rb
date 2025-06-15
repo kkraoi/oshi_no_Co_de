2e62252a58a9064cb597714393878ad5359b0944
kkraoi <aaoooii219@gmail.com>
2025-06-15 05:53:42 +0000

    [Add]投稿モデル等作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :posts, dependent: :destroy
   
   validates :name, presence: true
   validates :password_confirmation, presence: true, if: -> { password.present? }
