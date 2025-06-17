b40bd66721752df4ea0b5e84a21d75f994ccd545
kkraoi <aaoooii219@gmail.com>
2025-06-17 03:11:15 +0000

    [Update]投稿系、バリデーション追記

--- app/models/code.rb
+++ app/models/code.rb
@@ -1,4 +1,7 @@
 class Code < ApplicationRecord
   belongs_to :post
   belongs_to :language
+
+  validates :name, presence: true
+  validates :content, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -10,6 +10,8 @@ class Post < ApplicationRecord
   # allow_destroy: true: 子モデル（Code）に対して、「削除」のチェックを付けられるようにする。「<%= code_fields.check_box :_destroy %>」で削除フラグを送れる
   accepts_nested_attributes_for :codes, allow_destroy: true
 
+  validates :title, presence: true
+  
   # 指定された日時属性を日本時間で「YYYY/MM/DD」形式に整形して返す
   #
   # @param attribute [Symbol] 整形対象の日付属性。デフォルトは :updated_at
