6ee8d2b1afae384827377e17a62b9617dfec6cb3
kkraoi <aaoooii219@gmail.com>
2025-06-18 09:57:04 +0000

    [Update] グループ一覧一旦調整

--- app/controllers/public/groups_controller.rb
+++ app/controllers/public/groups_controller.rb
@@ -1,2 +1,49 @@
-class Public::GroupsController < ApplicationController
+class Public::GroupsController < Public::BaseController
+  # 他人のアクセス防止
+  before_action :ensure_correct_user, only: [:update, :edit, :destroy]
+
+  def index
+    @group = Group.all
+  end
+  
+  def show
+    
+  end
+  
+  def new
+    
+  end
+  
+  def create
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    
+  end
+  
+  def destroy
+    
+  end
+  
+  private
+
+  def group_params
+    params.require(:group).permit(
+      :name,
+      :introduction
+    )
+  end
+
+  # ユーザをチェック
+  def ensure_correct_user
+    @group = Group.find(params[:id])
+    unless @group.owner_id == current_user.id
+      redirect_to groups_path, alert: "アクセスを禁止しています"
+    end
+  end
 end
