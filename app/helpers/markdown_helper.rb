b1ad502ce2ec67a98bf36e2a3c971f6727800fa7
kkraoi <aaoooii219@gmail.com>
2025-06-19 08:32:06 +0000

    [Test]マークダウンヘルパーテスト

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -11,9 +11,6 @@ module MarkdownHelper
   def markdown_to_html(text)
     return "" if text.blank?
 
-    Kramdown::Document.new(
-      text,
-      input: 'GFM'
-    ).to_html.html_safe
+    
   end
 end
