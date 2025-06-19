809f312a6efe89cedc7a122fddb0d4943776c073
kkraoi <aaoooii219@gmail.com>
2025-06-19 07:59:05 +0000

    [Fix]本番でヘルパーが効かない問題対応

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -4,6 +4,8 @@
 # - メソッドはビュー内で呼び出せる
 # - 自動で読み込まれる（Railsが内部で ApplicationHelper を継承・include してくれる）
 # RailsはApplicationController → ApplicationHelper → その他のヘルパーという形で読み込む
+
+require 'kramdown'
  
 module MarkdownHelper
   def markdown_to_html(text)
