bba13d5de41535613f5f21bc2c4a4a06f2b52907
kkraoi <aaoooii219@gmail.com>
2025-06-19 08:41:06 +0000

    [Test]

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -10,6 +10,6 @@ require 'kramdown'
 module MarkdownHelper
   def markdown_to_html(text)
     return "" if text.blank?
-    Kramdown::Document.new("text").to_html.html_safe
+    Kramdown::Document.new(text).to_html.html_safe
   end
 end
