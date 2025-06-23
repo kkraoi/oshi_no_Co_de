24dfb3ffa90f760b950e45b38305d6fb1f2286eb
kkraoi <aaoooii219@gmail.com>
2025-06-23 15:53:38 +0000

    [Add]ビュー作成

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -1,2 +1,19 @@
 class Admin::ReportsController < ApplicationController
+  def index
+    
+  end
+  
+  def update
+    
+  end
+  
+  def feedback
+    
+  end
+  
+  def hide_comment
+    
+  end
+  
+  private
 end
