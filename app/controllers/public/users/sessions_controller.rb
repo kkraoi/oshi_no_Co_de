8a1ad4299a48448329e9c3c6bc18698f76bbaa6e
kkraoi <aaoooii219@gmail.com>
2025-06-24 14:51:10 +0000

    [Update]各ビュー: ゲストログインの制限

--- app/controllers/public/users/sessions_controller.rb
+++ app/controllers/public/users/sessions_controller.rb
@@ -2,6 +2,6 @@ class Public::Users::SessionsController < Devise::SessionsController
   def guest_sign_in
     user = User.guest
     sign_in user # ゲストユーザーをログイン状態にする
-    redirect_to user_path(user), notice: 'guestuserでログインしました'
+    redirect_to posts_path, notice: 'guestuserでログインしました'
   end
 end
