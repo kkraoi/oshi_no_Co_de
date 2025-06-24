74170f2315106187fff871716e14701e2907df2b
kkraoi <aaoooii219@gmail.com>
2025-06-24 13:49:06 +0000

    [Update]ゲストログインのためモデル調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,4 +31,14 @@ class User < ApplicationRecord
   def self.ransackable_attributes(auth_object = nil)
     %w[name created_at]
   end
+
+  GUEST_USER_EMAIL = "guest@example.com"
+
+  # Userモデルで使用できるメソッドとしてUser.guestの記述が可能になる
+  def self.guest
+    find_or_create_by!(email: GUEST_USER_EMAIL) do |user|
+      user.password = SecureRandom.urlsafe_base64 # ランダムな文字列を生成するRubyのメソッドの一種
+      user.name = "guestuser"
+    end
+  end
 end
