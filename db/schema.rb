8bd55f13df4489dabce0ab0575f6a7951b427d95
kkraoi <aaoooii219@gmail.com>
2025-06-25 11:56:21 +0000

    [Update]投稿モデルに属性追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_06_25_014416) do
+ActiveRecord::Schema.define(version: 2025_06_25_115437) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -116,6 +116,7 @@ ActiveRecord::Schema.define(version: 2025_06_25_014416) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "likes_count", default: 0, null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
