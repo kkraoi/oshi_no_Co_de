bc159b3db2fcadf4729f72dbcc4e8d7bbbcfd25e
kkraoi <aaoooii219@gmail.com>
2025-06-25 11:28:14 +0000

    [Update]いいね非同期

--- app/controllers/public/likes_controller.rb
+++ app/controllers/public/likes_controller.rb
@@ -1,15 +1,19 @@
 class Public::LikesController < Public::BaseController
   def create
-    post = Post.find(params[:post_id])
-    like = current_user.likes.new(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    like = current_user.likes.new(post_id: @post.id)
     like.save
-    redirect_to request.referer
+    respond_to do |format|
+      format.js
+    end
   end
   
   def destroy
-    post = Post.find(params[:post_id])
-    like = current_user.likes.find_by(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    like = current_user.likes.find_by(post_id: @post.id)
     like.destroy
-    redirect_to request.referer
+    respond_to do |format|
+      format.js
+    end
   end
 end
