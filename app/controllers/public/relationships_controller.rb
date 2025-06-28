f87f09300d58dd9fc920316fe99ccb6d45779153
kkraoi <aaoooii219@gmail.com>
2025-06-28 05:42:14 +0000

    [Update] 自身にフォローできないよう調整

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -1,13 +1,25 @@
 class Public::RelationshipsController < Public::BaseController
   def create
     user = User.find(params[:user_id])
-    current_user.follow(user)
-    redirect_to request.referer
+
+    if current_user != user
+      current_user.follow(user)
+    else
+      flash[:alert] = "自分自身をフォローすることはできません"
+    end
+
+    redirect_to request.referer || root_path
   end
   
   def destroy
     user = User.find(params[:user_id])
-    current_user.unfollow(user)
-    redirect_to  request.referer
+
+    if current_user != user
+      current_user.unfollow(user)
+    else
+      flash[:alert] = "自分自身をアンフォローすることはできません"
+    end
+
+    redirect_to request.referer || root_path
   end
 end
