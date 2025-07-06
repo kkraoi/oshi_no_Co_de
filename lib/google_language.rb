9e23528d20f8cefe5afcb63b86e6a8a778d05495
kkraoi <aaoooii219@gmail.com>
2025-07-06 08:42:01 +0000

    [Update]エンティティ分析を返すメソッド作成

--- lib/google_language.rb
+++ lib/google_language.rb
@@ -4,13 +4,16 @@ require "net/https"
 # ↑ これらはRubyに標準的に組み込まれているライブラリ
 
 module GoogleLanguage
+  END_POINT = "https://language.googleapis.com"
+  KEY_QUERY = "?key=#{ENV['GOOGLE_API_KEY']}"
+
   class << self
     def get_sentiment_data(text)
       # 感情分析用のAPIのURLを作成
       # エンドポイント: https://language.googleapis.com/
       # リソース: v1/documents:analyzeSentiment
       # クエリ: ?key=APIキー
-      api_url = "https://language.googleapis.com/v1/documents:analyzeSentiment?key=#{ENV['GOOGLE_API_KEY']}"
+      api_url = "#{END_POINT}/v1/documents:analyzeSentiment#{KEY_QUERY}"
 
       # APIリクエストの本文（JSON）
       # https://cloud.google.com/natural-language/docs/reference/rest/v1/documents/analyzeSentiment?hl=ja
@@ -21,6 +24,32 @@ module GoogleLanguage
         }
       }.to_json
 
+      # dig() => response_body['documentSentiment']['score']と同じ感じ
+      send_request(api_url, params)&.dig("documentSentiment", "score")
+    end
+
+    def get_entity_data(html)
+      api_url = "#{END_POINT}/v1/documents:analyzeEntities#{KEY_QUERY}"
+
+      params = {
+        "document": {
+          type: "HTML",
+          content: html
+        }
+      }.to_json
+
+      send_request(api_url, params)
+    end
+
+    private
+
+    # 指定された Google Cloud Natural Language API のエンドポイントに、JSON形式のPOSTリクエストを送信し、レスポンスをパースして返す。
+    #
+    # @param api_url [String] APIのエンドポイントURL（APIキー付き）
+    # @param params [String] JSON形式のリクエスト本文
+    # @return [Hash] パース済みのレスポンスボディ（Hash形式）
+    # @raise [RuntimeError] APIがエラーを返した場合、そのメッセージを例外として発生させる
+    def send_request(api_url, params)
       # HTTPの雛形を作成
       uri = URI.parse(api_url)
       https = Net::HTTP.new(uri.host, uri.port)
@@ -28,18 +57,18 @@ module GoogleLanguage
 
       # リクエスト作成
       request = Net::HTTP::Post.new(uri.request_uri)
-      request['Content-Type'] = 'application/json'
+      request["Content-Type"] = "application/json"
 
       # リクエストを送信し、レスポンスを受ける（ブロッキングメソッド）
       response = https.request(request, params)
 
       # レスポンスを整形する
       response_body = JSON.parse(response.body)
-      if (error = response_body['error']).present?
-        raise error['message']
+      if (error = response_body["error"]).present?
+        raise error["message"]
       else
-        response_body['documentSentiment']['score']
-      end 
+        response_body
+      end
     end
   end
 end
