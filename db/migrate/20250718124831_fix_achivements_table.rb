7aa43fac226dd621dd5e097a21e9276268520b6c
kkraoi <aaoooii219@gmail.com>
2025-07-22 10:48:17 +0000

    [Fix]実績テーブル削除

--- db/migrate/20250718124831_fix_achivements_table.rb
+++
@@ -1,7 +0,0 @@
-class FixAchivementsTable < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :achievements, :false_id, :integer
-
-    change_column_null :achievements, :user_id, false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_07_21_155820) do
-
-  create_table "achievements", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "title", null: false
-    t.text "description"
-    t.string "link"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_achievements_on_user_id"
-  end
+ActiveRecord::Schema.define(version: 2025_07_06_103205) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -180,8 +170,6 @@ ActiveRecord::Schema.define(version: 2025_07_21_155820) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "achievements", "users"
-  add_foreign_key "achievements", "users"
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "codes", "languages"
