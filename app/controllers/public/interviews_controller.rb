484f8bc2000d9f9c7957c2b60c2e089179fdc876
kkraoi <aaoooii219@gmail.com>
2025-07-27 10:40:29 +0000

    [Update]質問作成の仕組みを作成

--- app/controllers/public/interviews_controller.rb
+++ app/controllers/public/interviews_controller.rb
@@ -2,15 +2,26 @@ class Public::InterviewsController < Public::BaseController
   # ゲストユーザー制限
   include GuestUserRestriction
 
-    # 他人のアクセス防止
-    before_action :ensure_correct_user, only: [:new, :update, :edit, :destroy, :gacha]
+  # 他人のアクセス防止
+  before_action :ensure_correct_user, only: [:new, :update, :edit, :destroy, :gacha]
 
   def new
-    
+    @interview_list = Interview.where(user: [nil, current_user]);
+    @interview = @user.interviews.new
   end
   
   def create
-    
+    @user = User.find(params[:user_id])
+    @interview = @user.interviews.new(interview_params)
+
+    if @interview.save
+      respond_to do |format|
+        format.html { redirect_to new_user_interview(@user), notice: "面接質問を作成しました" }
+        format.js
+      end
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
   
   def edit
@@ -30,12 +41,17 @@ class Public::InterviewsController < Public::BaseController
   end
   
 private
-
   # ユーザをチェック
   def ensure_correct_user
     @user = User.find(params[:user_id])
     unless @user == current_user
       redirect_to user_path(current_user), alert: "アクセスを禁止しています"
     end
+  end
+
+  def interview_params
+    params.require(:interview).permit(
+      :content,
+    )
   end
 end
